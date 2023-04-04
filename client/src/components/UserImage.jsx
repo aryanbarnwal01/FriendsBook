8da4180f446bc58b7ea0655260c89025fb1ae1b5
Aryan Barnwal <77083847+aryanbarnwal01@users.noreply.github.com>
2023-04-04 14:32:12 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -6,7 +6,7 @@ const UserImage = ({ image, size = "60px" }) => {
     <img style={{ objectFit: "cover", borderRadius: "50%" }}
     width={size}
     height={size}
-    alt="user" src={`https://react-friendsbook.onrender.com/assets/${image}`} />
+    alt="user" src={`%PUBLIC_URL%/assets/${image}`} />
     
     </Box>
   );
