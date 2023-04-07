a5b25273862fd7469d76b2a3a1ff6170f88d8f97
Aryan Barnwal <77083847+aryanbarnwal01@users.noreply.github.com>
2023-04-07 11:59:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,3 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
@@ -10,36 +7,24 @@ import path from "path";
 import { fileURLToPath } from "url";
 import {register} from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js"
-
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import { createPost} from "./controllers/posts.js";
  
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({policy:"cross-origin"}));
-
-
-app.use((req, res, next) => {
-  res.removeHeader("Cross-Origin-Embedder-Policy");
-  next();
-});
+// app.use(helmet.crossOriginEmbedderPolicy({policy:"cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
-
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
-
-
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
         cb(null,"public/assets");
@@ -50,27 +35,20 @@ const storage = multer.diskStorage({
     
 });
 const upload = multer({storage});
-
-
-
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
-
 //Routes
 app.use("/auth",authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
-
-
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 const PORT = process.env.PORT || 6001;
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,    
 }).then(() => {
-    app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+    app.listen(PORT, 'react-friendsbook.onrender.com' , () => console.log(`Server Port : ${PORT}`));
 }).catch((error) => console.log(`${error} did not connect`));
-
-
-
