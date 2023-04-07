f3d0e2d2822129d4108b2ed6915cb5279b6086c0
Aryan Barnwal <77083847+aryanbarnwal01@users.noreply.github.com>
2023-04-07 16:01:44 +0530

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://react-friendsbook.onrender.com/auth/register",
+      "https://friendsbook-cb73a.web.app/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://react-friendsbook.onrender.com/auth/login", {
+    const loggedInResponse = await fetch("https://friendsbook-cb73a.web.app/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
